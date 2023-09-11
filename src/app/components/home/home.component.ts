39225016301b2cc384f3dd0319ebd38b3c9c2851
Amappola <anam.porrasp@gmail.com>
2023-09-11 09:01:52 -0500

    Passing house information from home component to housingLocation component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from 'src/app/interfaces/housing-location';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl: string = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'someurl.com',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  };
 }
