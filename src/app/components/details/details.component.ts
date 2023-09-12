6b6196519a68874ec4a9f9e7cc8173861d2aff02
Amappola <anam.porrasp@gmail.com>
2023-09-12 10:37:49 -0500

    Creating the layout for the details component and setting up its data

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
 import { HousingLocation } from 'src/app/interfaces/housing-location';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-details',
@@ -11,7 +12,12 @@ import { HousingLocation } from 'src/app/interfaces/housing-location';
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingLocationId = -1;
+  locationIcon = faLocationDot;
+  housingService: HousingService = inject(HousingService);
+  houseData: HousingLocation | undefined;
+
   constructor() {
       this.housingLocationId = Number(this.route.snapshot.params['id']);
+      this.houseData = this.housingService.getHouseById(this.housingLocationId);
   }
 }
